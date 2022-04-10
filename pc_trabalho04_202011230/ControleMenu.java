91a874c96bfb8d31176b04a17802ce3861fa34b8
Gustavo <93795019+Gustheou@users.noreply.github.com>
2022-04-10 12:26:50 -0300

    pc_trabalho_04

---
+++ pc_trabalho04_202011230/ControleMenu.java
@@ -0,0 +1,24 @@
+/* ***************************************************************
+* Autor............: Gustavo Pereira Nunes
+* Matricula........: 202011230
+* Inicio...........: 29/03/2022
+* Ultima alteracao.: 09/04/2022
+* Nome.............: ControleMenu
+* Funcao...........: Controlar a tela inicial
+*************************************************************** */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ControleMenu {
+
+/* ***************************************************************
+* Metodo: prosseguirButton
+* Funcao: trocar a tela inicial para a tela secundaria
+* Parametros: ActionEvent event = evento que requer uma acao para ser executado, que no caso eh o de apertar um botao
+* Retorno: void
+*************************************************************** */
+  @FXML
+  void prosseguirButton(ActionEvent event) {
+    Principal.changeScreenEntrega(event);
+  }//Fim do metodo iniciarButton
+}//Fim da classe ControleMenu
