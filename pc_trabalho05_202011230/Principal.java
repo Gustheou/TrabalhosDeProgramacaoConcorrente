b44248262c8ecf35ad9e6ed216ced721c7b1d856
Gustavo <gustavopn117@gmail.com>
2022-04-23 09:39:14 -0300

    pc_trabalho05

---
+++ pc_trabalho05_202011230/Principal.java
@@ -0,0 +1,62 @@
+/* ***************************************************************
+* Autor............: Gustavo Pereira Nunes
+* Matricula........: 202011230
+* Inicio...........: 13/04/2022
+* Ultima alteracao.: 22/04/2022
+* Nome.............: Principal
+* Funcao...........: O programa tem como funcao simular o problema do jantar dos filosofos
+*************************************************************** */
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Principal extends Application{
+  private static Stage stage;
+  private static Scene telaMenu;
+  private static Scene telaPrograma;
+
+/* ***************************************************************
+* Metodo: main
+* Funcao: lançar o programa
+* Parametros: args=essencial para tornar o arquivo como principal
+* Retorno: void
+*************************************************************** */
+  public static void main (String [] args) {
+    launch (args);
+  }//Fim do metodo main
+
+/* ***************************************************************
+* Metodo: start
+* Funcao: Iniciar a exibição de telas
+* Parametros: cenario=responsavel por permitir o uso de telas
+* Retorno: void
+*************************************************************** */
+  @Override
+  public void start (Stage cenario) throws IOException {
+    stage = cenario;
+    cenario.setTitle("Food wars");
+    Parent fxmlTelaInicial = FXMLLoader.load(getClass().getResource("TelaInicial.fxml"));
+    telaMenu = new Scene (fxmlTelaInicial);
+    Parent fxmlEntrega = FXMLLoader.load(getClass().getResource("TelaPrograma.fxml"));
+    telaPrograma = new Scene (fxmlEntrega);
+
+    cenario.getIcons().add(new Image("Icon.png"));
+    cenario.setScene(telaMenu);
+    cenario.show();
+  }//Fim do metodo start
+
+/* ***************************************************************
+* Metodo: changeScreenJantar
+* Funcao: trocar a tela inicial para a tela secundaria
+* Parametros: ActionEvent event = evento que requer uma acao para ser executado, que no caso eh o de apertar um botao
+* Retorno: void
+*************************************************************** */
+  public static void changeScreenJantar (ActionEvent event) {
+    stage.setScene(telaPrograma);
+  }//Fim do metodo changeScreenJantar
+}//Fim da classe Principal
